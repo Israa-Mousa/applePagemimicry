f661b8a5d3be5d49fea9a90daf68d756a6fb7547
Israa Mousa <israa.mazaraa@yahoo.com>
2022-05-14 09:26:26 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const selectElement=(element)=>document.querySelector(element);
+selectElement('.mobile-menu').addEventListener('click',()=>{
+    console.log('sjakhsahdj');
+
+selectElement('header').classList.toggle('active');
+
+});
